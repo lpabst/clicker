1f64870751c37754613a4a88c7249c1029d0fecd
loren pabst <lorenpabst@gmail.com>
2018-03-10 12:09:56 -0700

    moved shop items into an array on state

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,26 +6,50 @@ import './Home.css';
 class Home extends Component {
   constructor(props){
     super(props);
+
+    this.userClick = this.userClick.bind(this);
+    this.toggleShop = this.toggleShop.bind(this);
+    this.buyAutoInfecter = this.buyAutoInfecter.bind(this);
+    this.buyInfectedNeedles = this.buyInfectedNeedles.bind(this);
+    this.buyFasterInfectionRate = this.buyFasterInfectionRate.bind(this);
+
     this.state = {
-      score: 0,
+      score: 80,
       showShop: false,
       autoInfect: false,
       autoInfectorIncrementer: 1,
       autoInfectorInterval: 1500,
       infectedNeedles: false,
       fasterInfectionRate: false,
+      shopItems: [
+        {
+          title: 'Auto Infect',
+          click: this.buyAutoInfecter,
+          show: 'autoInfect',
+          cost: 20,
+        },
+        {
+          title: 'Buy Infected Needles',
+          click: this.buyInfectedNeedles,
+          show: 'infectedNeedles',
+          cost: 50,
+        },
+        {
+          title: 'Faster Infection Rate',
+          click: this.buyFasterInfectionRate,
+          show: 'fasterInfectionRate',
+          cost: 50,
+        }
+      ]
     }
 
-    this.updateScore = this.updateScore.bind(this);
-    this.toggleShop = this.toggleShop.bind(this);
-    this.buyAutoInfecter = this.buyAutoInfecter.bind(this);
-    this.buyInfectedNeedles = this.buyInfectedNeedles.bind(this);
-    this.buyfasterInfectionRate = this.buyfasterInfectionRate.bind(this);
   }
 
-  updateScore(newVal){
+  userClick(){
+    let newVal = this.state.score + this.state.autoInfectorIncrementer;
     this.setState({
-      score: newVal
+      score: newVal,
+      showShop: false,
     })
   }
 
@@ -62,7 +86,7 @@ class Home extends Component {
     }
   }
 
-  buyfasterInfectionRate(e){
+  buyFasterInfectionRate(e){
     e.stopPropagation();
     if (this.state.score >= 50){
       this.setState({
@@ -83,18 +107,21 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className='home_wrapper' onClick={(e) => this.updateScore(this.state.score + 1)}>
+        <div className='home_wrapper' onClick={this.userClick}>
 
           <p className='score'>Score: {this.state.score}</p>
           <div className='shop_btn btn' onClick={this.toggleShop}>Shop</div>
 
           {
             this.state.showShop ? 
-            <div className='shop_wrapper'>
-              {!this.state.autoInfect ? <p onClick={this.buyAutoInfecter}>$20 - Auto Infect</p> : null}
-              {!this.state.infectedNeedles ? <p onClick={this.buyInfectedNeedles}>$50 - Buy Infected Needles</p> : null}
-              {!this.state.fasterInfectionRate ? <p onClick={this.buyfasterInfectionRate}>$50 - Faster Infection Rate</p> : null}
-            </div>
+              <div className='shop_wrapper'>
+                {
+                  this.state.shopItems.map( (item, i) => {
+                    let color = this.state.score >= item.cost ? 'green' : 'red';
+                    return !this.state[item.show] ? <p key={i} onClick={item.click} style={{color: color}} className='shop_item' >${item.cost} - {item.title}</p> : null
+                  })
+                }
+              </div>
             : null
           }
 
