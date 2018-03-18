0cb85c1c16346214eb824b084d8558b799a3d1df
loren pabst <lorenpabst@gmail.com>
2018-03-17 21:03:41 -0600

    formatted money and INC

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -499,12 +499,12 @@ class Home extends Component {
   getNumInfected(){
     let num = this.state.numInfected;
 
-    if (num > 1000000000){
-      return (num/1000000000).toFixed(2) + ' Billion';
-    }else if (num > 1000000){
-      return (num/1000000).toFixed(2) + ' Million';
-    }else if (num > 1000){
-      return (num/1000).toFixed(2) + ' Thousand';
+    if (num >= 1000000000){
+      return (num/1000000000) + ' Billion';
+    }else if (num >= 1000000){
+      return (num/1000000) + ' Million';
+    }else if (num >= 1000){
+      return (num/1000) + ' Thousand';
     }else{
       return num;
     }
--- src/components/Home/HomeComponents/Info.js
+++ src/components/Home/HomeComponents/Info.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 
 class Info extends Component {
+
+    formatNumber(amt){
+        return amt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
     render() {
         let inc = Math.floor(this.props.autoInfectorIncrementer);
 
@@ -14,8 +19,8 @@ class Info extends Component {
                 <p className='timer'>Time To Infect One Million: {this.props.timeMillion}</p>
                 <p className='timer'>Time To Infect One Billion: {this.props.timeBillion}</p>
                 <p className='timer'>Time To Infect The Whole World: {this.props.timeGameOver}</p>
-                <p className='score'>Money: ${this.props.money.toFixed(2)}</p>
-                <p className='timer'>Incrementer (INC): {inc + (inc > 1 ? ' People' : ' Person')} infected at a time</p>
+                <p className='score'>Money: ${this.formatNumber(this.props.money)}</p>
+                <p className='timer'>Incrementer (INC): {this.formatNumber(inc) + (inc > 1 ? ' People' : ' Person')} infected at a time</p>
                 <p className='timer'>Interval (Rate): {(this.props.autoInfectorInterval / 1000).toFixed(3)} seconds</p>
             </section>
         );
